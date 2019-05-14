010424fb2d9704b7990a0735ff3a4d5673803346
Nick Annen <nannen05@gmail.com>
2019-05-13 18:01:35 -0700

    updating ports

--- src/components/socket.js
+++ src/components/socket.js
@@ -3,12 +3,14 @@ import io from "socket.io-client";
 let portUrl;
 
 if(process.env.PORT) {
-  portUrl = `https://protected-bastion-46350.herokuapp.com/:${process.env.PORT}`
+  portUrl = `https://protected-bastion-46350.herokuapp.com:${process.env.PORT}`
 } else {
   portUrl = process.env.REACT_APP_CURRENT_URL
 }
 
 export default function () {
+  console.log(portUrl)
+  console.log(process.env)
   const socket = io.connect(portUrl)
   
   function queryCard(cardInfo) {
